d8e9be020c239494eb982f9117eb3506dd44c7bc
Jeova BL <jeovab115@gmail.com>
2023-05-24 15:31:28 -0300

    otimização e novas fucionalidades

--- cafeteira.py
+++ cafeteira.py
@@ -9,96 +9,113 @@ chcolate = gramas
 copos = unidades 
 agua = ml
 """
-QuantidadeDeCafe = 1000
-QuantidadeDeleite = 1000
-QuantidadeDechocolate = 1000
-QuantidadeDeCopos = 50
-QuantidadeDeAgua = 1000
-cafeteira = Cafeteira(QuantidadeDeCafe, QuantidadeDeleite, QuantidadeDechocolate, QuantidadeDeCopos, QuantidadeDeAgua)
+
+cafeteira = Cafeteira( reservatorios["QuantidadeDeCafe"], reservatorios["QuantidadeDeleite"], reservatorios["QuantidadeDechocolate"], reservatorios["QuantidadeDeCopos"], reservatorios["QuantidadeDeAgua"])
 
 while True:
     cafeteira.displayMoney()
     try:
-        valor = float(input("Valor para adicionar na maquina: R$")) or float(0)
+        valor = float(input("Valor para adicionar na maquina: R$"))
         
     except ValueError:
         valor = 0
-    cafeteira.addmoney(valor=0)
+    cafeteira.addmoney(valor)
     cafeteira.displayMoney()
 
     escolha = cafeteira.escolhaBebidas()
 
     if escolha == 0:
-        cafeteira.display = f'{cafeteira.cafePo=}-{cafeteira.leite=}-{cafeteira.chocolate=}-{cafeteira.copos=}-{cafeteira.agua=}'
+        cafeteira.display = f'{cafeteira.cafePo=} {cafeteira.leite=} {cafeteira.chocolate=} {cafeteira.copos=} {cafeteira.agua=}'
         cafeteira.exibir_display()
         time.sleep(5)
         continue
     if escolha == 999:
+        if cafeteira.money > 0:
+            cafeteira.devolverDinheiroNaoUsado()
+            cafeteira.limparJanela()
+            cafeteira.encerramento()
+            time.sleep(3)
+            cafeteira.limparJanela()
+            cafeteira.desligando()
         break
 
 
     tem_ingredientes_e_copos = False 
+
     if escolha == 1:
         tem_ingredientes_e_copos = cafeteira.verificaCafeExpresso()
+        tem_ingredientes_e_copos = cafeteira.verificaCopos()
+
 
     elif escolha == 2:
-        if cafeteira.money < CAFE2:
-            cafeteira.display = "compra inválida: efetue um deposito"
-            cafeteira.exibir_display()
+        if cafeteira.money < precos_cafes["CAFE2"]:
+            cafeteira.limparJanela()
+            cafeteira.falta_dinheiro()
             continue
         tem_ingredientes_e_copos = cafeteira.verificaCafeLongo()
+        tem_ingredientes_e_copos = cafeteira.verificaCopos()
         if tem_ingredientes_e_copos == True:
-            cafeteira.money -= CAFE2
+            cafeteira.money -= precos_cafes["CAFE2"]
         
     elif escolha == 3:
-        if cafeteira.money < CAFE3:
+        if cafeteira.money < precos_cafes["CAFE3"]:
+            cafeteira.limparJanela()
+            cafeteira.falta_dinheiro()
             continue
         tem_ingredientes_e_copos = cafeteira.verificaCafePingado()
+        tem_ingredientes_e_copos = cafeteira.verificaCopos()
         if tem_ingredientes_e_copos == True:
-            cafeteira.money -= CAFE3
+            cafeteira.money -= precos_cafes["CAFE3"]
 
     elif escolha == 4:
-        if cafeteira.money < CAFE4:
+        if cafeteira.money < precos_cafes["CAFE4"]:
+            cafeteira.limparJanela()
+            cafeteira.falta_dinheiro()
             continue
         tem_ingredientes_e_copos = cafeteira.verificaCapuchino()
+        tem_ingredientes_e_copos = cafeteira.verificaCopos()
         if tem_ingredientes_e_copos == True:
-            cafeteira.money -= CAFE4
+            cafeteira.money -= precos_cafes["CAFE4"]
 
     elif escolha == 5:
-        if cafeteira.money < CAFE5:
+        if cafeteira.money < precos_cafes["CAFE5"]:
+            cafeteira.limparJanela()
+            cafeteira.falta_dinheiro()
             continue
         tem_ingredientes_e_copos = cafeteira.verificaCapChoc()
+        tem_ingredientes_e_copos = cafeteira.verificaCopos()
         if tem_ingredientes_e_copos == True:
-            cafeteira.money -= CAFE5
+            cafeteira.money -= precos_cafes["CAFE5"]
 
     elif escolha == 6:
-        if cafeteira.money < CAFE6:
+        if cafeteira.money < precos_cafes["CAFE6"]:
+            cafeteira.limparJanela()
+            cafeteira.falta_dinheiro()
             continue
         tem_ingredientes_e_copos = cafeteira.verificahocolate()
+        tem_ingredientes_e_copos = cafeteira.verificaCopos()
         if tem_ingredientes_e_copos == True:
-            cafeteira.money -= CAFE6
+            cafeteira.money -= precos_cafes["CAFE6"]
 
     elif escolha == 7:
-        if cafeteira.money < CAFE7:
+        if cafeteira.money < precos_cafes["CAFE7"]:
+            cafeteira.limparJanela()
+            cafeteira.falta_dinheiro()
             continue
         tem_ingredientes_e_copos = cafeteira.verificaCafeComChocolate()
+        tem_ingredientes_e_copos = cafeteira.verificaCopos()
         if tem_ingredientes_e_copos == True:
-            cafeteira.money -= CAFE7
+            cafeteira.money -= precos_cafes["CAFE7"]
 
     elif escolha == 8:
-        if cafeteira.money < CAFE8:
+        if cafeteira.money < precos_cafes["CAFE8"]:
+            cafeteira.limparJanela()
+            cafeteira.falta_dinheiro()
             continue
         tem_ingredientes_e_copos = cafeteira.verificaChocolateComLeite()
+        tem_ingredientes_e_copos = cafeteira.verificaCopos()
         if tem_ingredientes_e_copos == True:
-            cafeteira.money -= CAFE8
-
-    tem_ingredientes_e_copos = cafeteira.verificaCopos()
-
-    # elif escolha == 9:
-    #     if cafeteira.money < CAFE:
-    #         continue
-    #     cafeteira.money -= CAFE
-
+            cafeteira.money -= precos_cafes["CAFE8"]
 
     if tem_ingredientes_e_copos == True:
         os.system("cls")
@@ -113,14 +130,18 @@ while True:
             cafeteira.exibir_display()
             time.sleep(3)
 
-
-    elif tem_ingredientes_e_copos == False:
-
+    if tem_ingredientes_e_copos == False:
+        cafeteira.falta_igredientes()
+        cafeteira.limparJanela()
         if cafeteira.money > 0:
             cafeteira.devolverDinheiroNaoUsado()
             cafeteira.exibir_display()
             time.sleep(3)
             cafeteira.limparJanela()
+        cafeteira.encerramento()
+        cafeteira.limparJanela()
+        cafeteira.desligando()
+        break
 
     cafeteira.limparJanela()
     cafeteira.encerramento()
--- sistema.py
+++ sistema.py
@@ -1,15 +1,23 @@
 import os
 import time
 
-CAFE1 = 0
-CAFE2 = 0.50
-CAFE3 = 0.75
-CAFE4 = 1.00
-CAFE5 = 1.25
-CAFE6 = 1.50
-CAFE7 = 1.75
-CAFE8 = 2.00
-CAFE9 = 5.00
+precos_cafes = {
+    "CAFE1": 0,
+    "CAFE2": 0.50,
+    "CAFE3": 0.75,
+    "CAFE4": 1.00,
+    "CAFE5": 1.25,
+    "CAFE6": 1.50,
+    "CAFE7": 1.75,
+    "CAFE8": 2.00,
+}
+reservatorios = {
+    "QuantidadeDeCafe": 1000,
+    "QuantidadeDeleite": 1000,
+    "QuantidadeDechocolate": 1000,
+    "QuantidadeDeCopos": 1,
+    "QuantidadeDeAgua": 1000,
+}
 
 class Cafeteira:
     def __init__(self, cafe=0, leite=0, chocolate=0, copos=0, agua=0, money=00.00) -> None: 
@@ -24,16 +32,15 @@ class Cafeteira:
 
 
     def exibir_display(self):
-        cafe1 = f'1-Café expresso-R${CAFE1:.2f}'
-        cafe2 = f'2-Café longo-R${CAFE2:.2f}'
-        cafe3 = f'3-Café pingado-R${CAFE3:.2f}'
-        cafe4 = f'4-Capuchino-R${CAFE4:.2f}'
-        cafe5 = f'5-Cap-choc-R${CAFE5:.2f}'
-        cafe6 = f'6-Chocate-R${CAFE6:.2f}'
-        cafe7 = f'7-Café com chocoollate-R${CAFE7:.2f}'
-        cafe8 = f'8-Chocolate-com leite R${CAFE8:.2f}'
-        cafe9 = f''
-        # 9-opção customizada-R${CAFE9:.2f}
+        cafe1 = f'1-Café expresso-R${precos_cafes["CAFE1"]:.2f}'
+        cafe2 = f'2-Café longo-R${precos_cafes["CAFE2"]:.2f}'
+        cafe3 = f'3-Café pingado-R${precos_cafes["CAFE3"]:.2f}'
+        cafe4 = f'4-Capuchino-R${precos_cafes["CAFE4"]:.2f}'
+        cafe5 = f'5-Cap-choc-R${precos_cafes["CAFE5"]:.2f}'
+        cafe6 = f'6-Chocate-R${precos_cafes["CAFE6"]:.2f}'
+        cafe7 = f'7-Café com chocoollate-R${precos_cafes["CAFE7"]:.2f}'
+        cafe8 = f'8-Chocolate-com leite R${precos_cafes["CAFE8"]:.2f}'
+        desligar = f'999- para desligar a maquina'
         print("╔═════════════════════════════════════════════════════╗")
         print("║                   Maquina de café                   ║")
         print("╠═════════════════════════════════════════════════════╣")
@@ -44,130 +51,174 @@ class Cafeteira:
         print(f"║{cafe5.center(23)} {cafe6.center(29)}║")
         print(f"║{cafe7.center(53)}║")
         print(f"║{cafe8.center(53)}║")
-        print(f"║{cafe9.center(53)}║") 
+        print(f"║{desligar.center(53)}║")
         print("╚═════════════════════════════════════════════════════╝")
-
-
 
 
     def dispensadorCopos(self):
         self.display = 'Dispensando um copo'
-        self.exibir_display()
+        Cafeteira.exibir_display(self)
         time.sleep(3)
         Cafeteira.limparJanela(self)
 
 
     def aquecendoIngredientes(self):
         self.display = "esquentando os ingredientes"
-        self.exibir_display()
+        Cafeteira.exibir_display(self)
         time.sleep(5) 
         Cafeteira.limparJanela(self)
 
+
     def moerCafe(self):
         self.display = "Moendo o café"
-        self.exibir_display()
+        Cafeteira.exibir_display(self)
         time.sleep(5)
         Cafeteira.limparJanela(self)
     
+
     def servirCafe(self):
         self.display = "Servindo café..."
-        self.exibir_display()
+        Cafeteira.exibir_display(self)
         time.sleep(5)
         Cafeteira.limparJanela(self)
         self.display = "Café servido✓"
-        self.exibir_display()
+        Cafeteira.exibir_display(self)
         time.sleep(5)
         Cafeteira.limparJanela(self)
 
-################################################################
-    def verificaCafeExpresso(self):
-        if self.cafePo >= 75 and self.agua >= 150:
-            self.cafePo -= 75
-            self.agua-= 150
-            return True        
-    def verificaCafeLongo(self):
-        if self.cafePo >= 20 and self.leite >= 60 and self.agua >= 60:
-            self.cafePo -= 150
-            self.leite -= 30
-            self.agua -= 120
-            return True
-    def verificaCafePingado(self):
-        if self.cafePo >= 10 and self.leite >= 100 and self.agua >= 40:
-            self.cafePo -= 150
-            self.leite -= 135
-            self.agua -= 20
-            return True
-    def verificaCapuchino(self):
-        if self.cafePo >= 20 and self.agua >= 30 and self.leite >= 100: 
-            self.cafePo -= 150
-            self.agua -= 30
-            self.leite -= 100
-            return True
-    def verificaCapChoc(self):
-        if self.cafePo >= 10 and self.leite >= 130 and self.chocolate >= 10:
-            self.cafePo -= 100
-            self.leite -= 150
-            self.chocolate -= 150
-            return True
-    def verificaCafeComChocolate(self):
-        if self.cafePo >= 40 and self.chocolate >= 100 and self.agua >= 130:
-            self.cafePo -= 40
-            self.agua -= 130
-            self.chocolate -= 100
-            return True
-    def verificahocolate(self):
-        if self.chocolate >=100 and self.agua >= 140:
-            self.chocolate -= 100
-            self.agua -= 140
-            return True
-    def verificaChocolateComLeite(self):
-        if self.chocolate >= 80 and self.leite >= 140 :
-            self.chocolate -= 80
-            self.leite -= 140
-            return True
-    def verificaCopos(self):
-        if self.copos > 0:
-            self.copos -= 1
-            return True
-################################################################
 
     def devolvendoTroco(self):
         self.display = f'Devolvendo o seu troco de R${self.money:.2f}'
         self.money = 0
     
+
     def devolverDinheiroNaoUsado(self):
         self.display = f'Devolvendo o se dinhiero R${self.money:.2f}'
+        Cafeteira.exibir_display(self)
         time.sleep(3)
         self.money = 0
     
+
     def naoTemIngredientes(self):
         Cafeteira.limparJanela(self)
         self.display = 'Ingrendientes em falta✕'
-        self.exibir_display()
+        Cafeteira.exibir_display(self)
         time.sleep(5)
         Cafeteira.limparJanela(self)
     
+
     def encerramento(self):
         self.display = 'Obrigado por usar nosso serviço!'
-        self.exibir_display()
+        Cafeteira.exibir_display(self)
         time.sleep(3)
     
+
     def limparJanela(self):
         os.system('cls')
     
+
     def displayMoney(self):
         self.limparJanela()
         self.display = f'RS:{self.money:.2f}'
-        self.exibir_display()
+        Cafeteira.exibir_display(self)
     
-    def addmoney(self, valor):
+
+    def addmoney(self, valor=0):
         self.money += valor
+
 
     def escolhaBebidas(self):
         while True:
             escolha = input("Escolha a sua bebida: ")
             if escolha in '123456789990':
                 return int(escolha)
+      
             
-    
-    
+    def falta_dinheiro(self):
+        self.display = "compra inválida: valor insuficiente"
+        Cafeteira.exibir_display(self)
+        time.sleep(3)
+        Cafeteira.limparJanela(self)
+
+
+    def falta_igredientes(self):
+        Cafeteira.limparJanela(self)
+        self.display = "compra inválida: Falta ingredientes"
+        Cafeteira.exibir_display(self)
+        time.sleep(3)
+        Cafeteira.limparJanela(self)
+        self.display = "contate o gerente para reabastecer"
+        Cafeteira.exibir_display(self)
+        time.sleep(3)
+        Cafeteira.limparJanela(self)
+
+
+    def desligando(self):
+        self.display = "Desligando..."
+        Cafeteira.exibir_display(self)
+        time.sleep(3)
+        Cafeteira.limparJanela(self)
+        self.display = " "
+        Cafeteira.exibir_display(self)
+################################################################
+    def verificaCafeExpresso(self):
+        if self.cafePo >= 75 and self.agua >= 150:
+            self.cafePo -= 75
+            self.agua-= 150
+            return True  
+              
+    def verificaCafeLongo(self):
+        if self.cafePo >= 20 and self.leite >= 60 and self.agua >= 60:
+            self.cafePo -= 150
+            self.leite -= 30
+            self.agua -= 120
+            return True
+        
+    def verificaCafePingado(self):
+        if self.cafePo >= 10 and self.leite >= 100 and self.agua >= 40:
+            self.cafePo -= 150
+            self.leite -= 135
+            self.agua -= 20
+            return True
+        
+    def verificaCapuchino(self):
+        if self.cafePo >= 20 and self.agua >= 30 and self.leite >= 100: 
+            self.cafePo -= 150
+            self.agua -= 30
+            self.leite -= 100
+            return True
+        
+    def verificaCapChoc(self):
+        if self.cafePo >= 10 and self.leite >= 130 and self.chocolate >= 10:
+            self.cafePo -= 100
+            self.leite -= 150
+            self.chocolate -= 150
+            return True
+        
+    def verificaCafeComChocolate(self):
+        if self.cafePo >= 40 and self.chocolate >= 100 and self.agua >= 130:
+            self.cafePo -= 40
+            self.agua -= 130
+            self.chocolate -= 100
+            return True
+        
+    def verificahocolate(self):
+        if self.chocolate >=100 and self.agua >= 140:
+            self.chocolate -= 100
+            self.agua -= 140
+            return True
+        
+    def verificaChocolateComLeite(self):
+        if self.chocolate >= 80 and self.leite >= 140 :
+            self.chocolate -= 80
+            self.leite -= 140
+            return True
+        
+    def verificaCopos(self):
+        if self.copos > 0:
+            self.copos -= 1
+            return True
+        else:
+            return False
+        
+################################################################
